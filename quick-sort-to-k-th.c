a1d4d430d0f5905c7ec30fe9b64a0f627daf6225
KaltZK <715480954@qq.com>
2016-03-21 23:52:50 +0800

    Kth DONE

--- quick-sort-to-k-th.c
+++ quick-sort-to-k-th.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-int a[]={6,5,4,3,2,1};
+int a[305];
 int n=sizeof(a)/sizeof(int);
 void swap(int *a,int *b){
 	int t;
@@ -17,28 +17,32 @@ void dispa(int n,int a[]){
 int qs(int begin,int end,int k,int a[]){
 	int key=a[begin];
 	int p=begin,q=end-1;
-	if(begin>=end-1) return a[k];
+	if(begin>=end-1){
+                assert(begin==k);
+                return a[begin];
+        }
 	assert(begin<end);
 	while(p<q){
 		while(p<q && a[q]>=key) q--;
 		swap(a+p,a+q);
-		while(p<q && a[p]<key) p++;
+		while(p<q && a[p]<=key) p++;
 		swap(a+p,a+q);
 	}
 	a[p]=key;
-	printf(">> p: %d a[p]: %d k:%d\n",p,a[p],k);
-	dispa(end-begin,a+begin);
-	if(p>k) 
+	//~ printf(">> p: %d a[p]: %d k: %d\n",p,a[p],k);
+	//~ dispa(end-begin,a+begin);
+	if(p>k)
 		return qs(begin,p+1,k,a);
 	else if(p<k)
-		return qs(p+1,end,k-p,a);
+		return qs(p+1,end,k,a);
 	return a[p];
 }
 int main(){
-	int k;
+	int n,k,i;
+	scanf("%d",&n);
+        for(i=0;i<n;i++)
+                scanf("%d",a+i);
 	scanf("%d",&k);
-	dispa(n,a);
-	printf("[ %d ]\n",qs(0,n,k,a));
-	dispa(n,a);
+	printf("%d\n",qs(0,n,k,a));
 	return 0;
 }
